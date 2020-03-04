7940ffaa36446343c01dd1d7d5677b4ec7a0d80d
Kamil Podsiadło <adres email>
2020-03-04 17:11:27 +0100

    add filter select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'live-filter-select';
+export class AppComponent implements OnInit {
+
+  users: any;
+  filteredUsers: any;
+
+  constructor(private api: ApiService) {
+
+  }
+
+  ngOnInit() {
+    this.api.getUsers().subscribe(users => {
+      this.users = users;
+      this.filteredUsers = users;
+    })
+  }
+
+  filterUsers(filteredText: string) {
+    this.filteredUsers = this.users.filter(user =>  user.name.toLowerCase().includes(filteredText));
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
